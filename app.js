c4e55fafd10db0749911a283c9ff6356e64b9a67
unknown <sharyarsheikh1122@gmail.com>
2023-12-16 06:05:23 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+function chng(){
+    var pra = document.getElementById(pra1)
+    pra1.style.color = 'red'
+}
+
+
+function chng1(){
+    var pra = document.getElementById(pra2)
+    pra2.style.color = 'green'
+}
+
+function chng2(){
+    var pra = document.getElementById(pra3)
+    pra3.style.color = 'blue'
+}
+
